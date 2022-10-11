4cf474a3f17455c564ac07300b602759ad7389e9
codyastubbs <5488363+codyastubbs@users.noreply.github.com>
2022-10-11 16:45:44 -0400

    Add layout, styling, and basic click interaction

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 const gameboard = (() => {
-    const row  =  ['', '', ''];
+    const row = ['', '', ''];
     return [row, row, row];
 })();
 
 const player = (name, mark) => {
-    return {name, mark};
+    return {
+        name,
+        mark
+    };
 }
 
 const playerOne = player("P1", "O");
 const playerTwo = player("P2", "X");
+
+let spaces = document.querySelectorAll(".column");
+spaces.forEach((space) => {
+    space.addEventListener('click', (e) => {
+        let row = space.parentElement.id;
+        let column = space.id;
+        gameboard[row][column] = 'X';
+        space.textContent = gameboard[row][column];
+    })
+})

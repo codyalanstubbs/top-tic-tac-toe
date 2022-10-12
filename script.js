325b2221f6d282bcc62efd9c4067eec56e7914b5
codyastubbs <5488363+codyastubbs@users.noreply.github.com>
2022-10-12 09:13:42 -0400

    Check if space has already been marked

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const gameboard = (() => {
-    const row = ['', '', ''];
-    return [row, row, row];
+    const firstRow = ['', '', ''];
+    const secondRow = ['', '', ''];
+    const thirdRow = ['', '', ''];
+    return [firstRow, secondRow, thirdRow];
 })();
 
 const player = (name, mark) => {
@@ -13,12 +15,21 @@ const player = (name, mark) => {
 const playerOne = player("P1", "O");
 const playerTwo = player("P2", "X");
 
+// Add event listeners to each space
+let message = document.querySelector(".message");
 let spaces = document.querySelectorAll(".column");
 spaces.forEach((space) => {
     space.addEventListener('click', (e) => {
+        message.textContent = ""; // Reset message on each click
+
         let row = space.parentElement.id;
         let column = space.id;
-        gameboard[row][column] = 'X';
-        space.textContent = gameboard[row][column];
+    
+        if (gameboard[row][column] === '') {
+            gameboard[row][column] = 'X';
+            space.textContent = gameboard[row][column];
+        } else {
+            message.textContent = "Hey! That space is already taken.";
+        }
     })
 })
